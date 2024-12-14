e4266655d20516e10377fd005f9b87d209eef7f3
Faheem Mohamed Rafi <169138733+Flydetective@users.noreply.github.com>
2024-12-14 12:42:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# MySQL Database Configuration
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Faheem18!"
+DB_NAME = "registration"
+
+# General 
+WINDOW_TITLE = "Person Registration Portal"
+WINDOW_SIZE = "500x500"
